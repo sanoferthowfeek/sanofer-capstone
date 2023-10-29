3d39694357d80f36d83522e154bb0a161f63a811
sanofer <mssanofer@gmail.com>
2023-10-29 18:43:04 +0530

    RECIPE

--- Recipe/Frontend/recipe-app/src/App.jsx
+++ Recipe/Frontend/recipe-app/src/App.jsx
@@ -2,24 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Base from './Base/Base.jsx'
-import Footer from './Base/Footer.jsx'
-import Sidebar from './Base/Sidebar.jsx'
-
-
-
-
-
+import Breakfast from './Menu/Breakfast.jsx'
 
 
 function App() {
- 
 
   return (
     <>
-     <Base />
-     <Sidebar />
-     <Footer />
+     <Breakfast />
        
     </>
   )
--- Recipe/Frontend/recipe-app/src/Pages/Home.jsx
+++ Recipe/Frontend/recipe-app/src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Base from '../Base/Base.jsx';
-
+import Sidebar from '../Base/Sidebar.jsx'
+import Footer from '../Base/Footer.jsx';
 const Home = () => {
   return (
+    <div>
 <Base title={"Welcome To Recipe App"}> 
+<Sidebar/> 
 <h2>Find Best Recipes For Cooking,Let's Cook.....</h2>
 <h4>Learn Quick Easy & Delicious Recipes...</h4>
 <h5>Yummy.......</h5>
 </Base>
+<Footer />
+</div>
+
   )
 }
 
