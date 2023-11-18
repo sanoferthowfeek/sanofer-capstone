1ba8b4dac52d7e056e3df4a19a298cfad89d597a
sanofer <mssanofer@gmail.com>
2023-11-19 01:45:56 +0530

    recipe

--- Recipe-book/Frontend/recipe-app/src/Pages/Home.jsx
+++ Recipe-book/Frontend/recipe-app/src/Pages/Home.jsx
@@ -21,8 +21,8 @@ return (
 <div className='home'>
 <Base />
 <h1 className='homehead'><b>Welcome To Recipe Book</b></h1><br />
-<h2><b><i>Find Best Recipes For Cooking,Let's Cook.....</i></b></h2><br />
-<h3><b><i>Learn Quick,Easy & Delicious Recipes...</i></b></h3><br />
+<h2><b><i>Find Best Recipes For Cooking.....</i></b></h2><br />
+<h3><b><i>Learn Easy & Delicious Recipes...</i></b></h3><br />
 <h4><b><i>Yummy.......<MoodRoundedIcon /></i></b></h4>
 
 <Footer />
